ad7260e87c9ad2990381673b0b8acdebf5e88ce0
Makram Jandar <makramjandar@gmail.com>
2019-06-24 21:06:26 +0200

    +

---
+++ dnn/__init__.py
@@ -0,0 +1,14 @@
+# __init__.py
+
+# @uthor: Makram Jandar
+#  |    |    \|    |      |     |    \|  |  |
+#   |  ||  _  ||  ||      |_____|  o  )  |  |
+#   |  ||  |  ||  ||_|  |_|     |   _/|  ~  |
+#   |  ||  |  ||  |  |  | |_____|  |  |___, |
+#   |  ||  |  ||  |  |  |       |  |  |     |
+#  |____|__|__|____| |__|       |__|  |____/ 
+#                     © init dnn directory...
+
+from dnn.activation import Activation as activate
+from dnn.initialization import Initialization as init
+#import numpy as np
---
+++ utils/__init__.py
@@ -0,0 +1,13 @@
+# __init__.py
+
+# @uthor: Makram Jandar
+#  |    |    \|    |      |     |    \|  |  |
+#   |  ||  _  ||  ||      |_____|  o  )  |  |
+#   |  ||  |  ||  ||_|  |_|     |   _/|  ~  |
+#   |  ||  |  ||  |  |  | |_____|  |  |___, |
+#   |  ||  |  ||  |  |  |       |  |  |     |
+#  |____|__|__|____| |__|       |__|  |____/ 
+#                   © init utils directory...
+
+from utils.helper_functions import reloadJupyter
+from utils.helper_functions import updateProgress
